c76458b8ae72e8d8d3c81e5aab7f39b44f8e32ff
Maharaj <mvp46@njit.edu>
2020-07-15 19:21:03 -0400

    Updated Mapping Profile

--- MappingProfile.cs
+++ MappingProfile.cs
@@ -18,6 +18,7 @@ namespace SchoolAPI
             CreateMap<User, UserDto>();
             CreateMap<CourseSectionMgt, CourseSectionDto>();
             CreateMap<Assignment, AssignmentDto>();
+            CreateMap<SecAssignmentMgt, SecAssignmentDto>();
             CreateMap<UserForCreationDto, User>();
             CreateMap<EnrollmentForCreationDto, SecEnrollmentMgt>();
             CreateMap<CourseForCreationDto, CourseMgt>();
@@ -25,6 +26,7 @@ namespace SchoolAPI
             CreateMap<UserForUpdateDto, User>();
             CreateMap<CourseSectionForUpdateDto, CourseSectionMgt>().ReverseMap(); 
             CreateMap<AssignmentForUpdateDto, Assignment>().ReverseMap();
+            CreateMap<SecAssignmentForUpdateDto, SecAssignmentMgt>().ReverseMap();
             CreateMap<CourseForUpdateDto, CourseMgt>();
         }
     }
